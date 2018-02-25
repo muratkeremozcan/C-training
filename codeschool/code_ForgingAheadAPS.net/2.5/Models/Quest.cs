af9c966abc1ad7a12baf172c4ef77c85bcb4f313
muratkerem <muratkerem@gmail.com>
2018-02-25 06:48:20 -0600

    finished course

--- codeschool/code_ForgingAheadAPS.net/2.5/Models/Quest.cs
+++ codeschool/code_ForgingAheadAPS.net/2.5/Models/Quest.cs
@@ -8,6 +8,7 @@ namespace ForgingAhead.Models
   {
     [Key]
     public string Name { get; set; }
+    [Display(Name = "Is Active")]
     public bool IsActive { get; set; }
     // set up relationship here
     public List<Character> Characters { get; set; }
